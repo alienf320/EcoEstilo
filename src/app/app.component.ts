269aa0dfe487f5223ebd04cfee8e908be90bad60
unknown <cmchavez@applaudostudios.com>
2023-04-06 19:26:20 -0300

    Version 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'EcoEstilo';
+  items = [
+    { url: '../assets/slideshow/2.jpeg'}, 
+    { url: '../assets/slideshow/3.jpeg'}, 
+    { url: '../assets/slideshow/4.jpeg'}, 
+    { url: '../assets/slideshow/5.jpeg'}, 
+    { url: '../assets/slideshow/6.jpeg'}, 
+    { url: '../assets/slideshow/7.jpeg'}, 
+
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarouselItemElementDirective } from './components/carousel/carousel-item-element.directive';
+import { CarouselItemDirective } from './components/carousel/carousel-item.directive';
+import { CarouselComponent } from './components/carousel/carousel.component';
+
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button';  
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    CarouselItemDirective,
+    CarouselItemElementDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
